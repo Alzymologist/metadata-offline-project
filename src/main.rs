4aa92fb849fc0a4bd914ecd7d49021c4af2fd30d
Slesarev <slesarew@gmail.com>
2023-09-01 12:31:00 +0300

    merkle tree oprimized for proof size on static data (perfect)

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,62 @@
+use rand::prelude::*;
+use merkle_cbt::{CBMT, merkle_tree::Merge};
+
+const TOTAL: u32 = 5000;
+const SUBSET: u32 = 50;
+
+struct MergeHashes {}
+
+impl Merge for MergeHashes {
+    type Item = [u8; 32];
+    fn merge(left: &Self::Item, right: &Self::Item) -> Self::Item {
+        blake3::hash(&[*left, *right].concat()).into()
+    }
+}
+
+type CBMTH = CBMT<[u8; 32], MergeHashes>;
 
 fn main() {
     let mut testbed: Vec<[u8; 32]> = Vec::new();
-    for i in 0..5000u32 {
+    for i in 0..TOTAL {
         testbed.push(blake3::hash(&i.to_le_bytes()).into());
     }
 
     testbed.sort();
 
-    let mut hasher = blake3::Hasher::new();
-    let testbed_copy = testbed.clone();
-    for i in testbed_copy.iter() {
-        hasher.update(i);
+    let root = CBMTH::build_merkle_root(&testbed);
+    
+    println!("Merkle root is {:?}", root);
+   
+    let mut rng = rand::thread_rng();
+    let subset_i = rand::seq::index::sample(&mut rng, TOTAL as usize, SUBSET as usize).iter().map(|a| a as u32).collect::<Vec<u32>>();
+    println!("Included nodes: {:?}", subset_i);
+    let mut subset = Vec::new();
+    for i in subset_i.iter() {
+        subset.push(testbed[*i as usize]);
     }
+    //println!("{:?}", subset);
 
-    let root_one = hasher.finalize();//blake3::hash(&testbed.concat());
+    let proof = CBMTH::build_merkle_proof(&testbed, &subset_i).unwrap();
 
-    println!("{:?}", root_one);
+    println!("correct inclusion proof: {:?}", proof.verify(&root, &subset));
 
-    let mut chunks: Vec<blake3::Hash> = Vec::new();
-    for i in 0..testbed.len() {
-        chunks.push(blake3::guts::ChunkState::new((i as u64)).update(&testbed[i]).finalize(false).into());
-    }
+    let indices = proof.indices();
+    let lemmas = proof.lemmas();
 
-    let width = chunks.len();
+    println!("Proof indices number: {:?}", indices.len());
 
-    let depth = if width.is_power_of_two() {
-        testbed.len().ilog2()
-    } else {
-        testbed.len().ilog2() + 1
-    };
-    println!("{:?}", depth);
+    println!("Proof lemmas number: {:?}", lemmas.len());
 
-    let mut old_collector: Vec<blake3::Hash> = chunks; //for this to work, chunks should be 1kb in
-                                                       //size; there are projects that make chunks
-                                                       //larger, not the opposite. No benefit over
-                                                       //special merkle tree crate
-    for i in 0..depth {
-        let mut new_collector: Vec<blake3::Hash> = Vec::new();
-        for j in 0..old_collector.len()/2 {
-            new_collector.push(blake3::guts::parent_cv(&old_collector[j*2], &old_collector[j*2+1], i==depth-1));
-        }
-        if old_collector.len().trailing_zeros() == 0 {
-            if let Some(a) = old_collector.last() {    
-                new_collector.push(*a);
-            }
-        }
-        old_collector = new_collector;
+    println!("Proof indices size: {:?}", indices.len()*4);
 
-    }
+    println!("Proof lemmas size: {:?}", lemmas.len()*32);
 
-    println!("{:?}", old_collector);
+    println!("Total overhead size: {:?}", indices.len()*4 + lemmas.len()*32);
+
+    subset[0] = [0; 32];
+
+    println!("incorrect inclusion proof: {:?}", proof.verify(&root, &subset));
+
+
 }
 
